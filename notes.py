e59ff5544abe39cea399c6d66850b4d18094cda6
Oleksandr_Skuz <138138803+OleksandrSkuz1@users.noreply.github.com>
2023-11-11 16:58:17 +0200

    created classes Note&NoteBook

--- notes.py
+++ notes.py
@@ -1,16 +1,18 @@
-class Notes():
-    def __init__(self):
-        self.notes = {} 
+from collections import UserDict
+from constants import RED, GRAY, CYAN, MAGENTA, RESET, LEN_OF_NAME_FIELD
+from datetime import datetime
+import os.path
+import pickle
+from classes import Field 
 
+
+class Note:
+    def __init__(self, title: str, content: str):
+        self.title = title
+        self.notes = content
+        
     def add_note(self, title, content):
         self.notes[title] = content 
-                                    
-    def search_notes(self, keyword): 
-        result = []
-        for title, content in self.notes.items():
-            if keyword.lower() in title.lower() or keyword.lower() in content.lower():
-                result.append(f"Title: {title}\nContent: {content}\n")
-        return result if result else "No matching notes found."
 
     def edit_note(self, title, new_content):
         if title in self.notes:
@@ -25,3 +27,17 @@ class Notes():
             return f"Note '{title}' deleted."
         else:
             return f"Note '{title}' not found."
+                
+    
+class NotesBook(UserDict):
+    def __init__(self):
+        self.notes = {} 
+
+                                    
+    def search_notes(self, keyword): 
+        result = []
+        for title, content in self.notes.items():
+            if keyword.lower() in title.lower() or keyword.lower() in content.lower():
+                result.append(f"Title: {title}\nContent: {content}\n")
+        return result if result else "No matching notes found."
+
